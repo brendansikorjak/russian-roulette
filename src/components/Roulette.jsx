79f913bc9f0377a26c084ced74a329c901ca6d21
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2024-02-07 17:59:37 -0500

    added basic game functionality

--- src/components/Roulette.jsx
+++ src/components/Roulette.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react';
 
 function RusRoulette() {
-  const [count, setCount] = useState(0);
+  const [bullet, setBullet] = useState(Math.floor(Math.random() * 6) + 1);
+  const [count, setCount] = useState(6);
+
+  const shoot = () => {
+    console.log(count);
+    console.log('the bullet is in chamber ' + bullet);
+    setCount(count - 1);
+    // setCount((count) => count + 1);
+    if (count === bullet) {
+      console.log('BANG');
+      setCount(6);
+      setBullet(Math.floor(Math.random() * 6) + 1);
+    } else {
+      console.log('click');
+    }
+  };
+
+  if (count === 0) {
+    setCount(6);
+  }
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={shoot}>count is {count}</button>
       </div>
     </>
   );
