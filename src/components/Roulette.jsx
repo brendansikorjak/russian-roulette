886660bdae5d520fe7d65c0e8be54580c97361f5
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2024-02-08 11:17:35 -0500

    added btnMsg, text, and liveGame hooks

--- src/components/Roulette.jsx
+++ src/components/Roulette.jsx
@@ -3,30 +3,43 @@ import { useState } from 'react';
 function RusRoulette() {
   const [bullet, setBullet] = useState(Math.floor(Math.random() * 6) + 1);
   const [count, setCount] = useState(6);
-
+  const [btnMsg, setBtnMsg] = useState('shoot gun');
+  const [text, setText] = useState(count + ' shots left');
+  const [liveGame, setLiveGame] = useState(true);
   const shoot = () => {
     console.log(count);
     console.log('the bullet is in chamber ' + bullet);
     setCount(count - 1);
+    setText(count - 1 + ' shots left');
     // setCount((count) => count + 1);
     if (count === bullet) {
       console.log('BANG');
-      setCount(6);
-      setBullet(Math.floor(Math.random() * 6) + 1);
+      setLiveGame(false);
+      setText('BANG');
+      setBtnMsg('Play Again?');
     } else {
       console.log('click');
+      setBtnMsg('shoot gun');
     }
   };
-
+  if (liveGame === false) {
+    setLiveGame(true);
+    setCount(6);
+    setBullet(Math.floor(Math.random() * 6) + 1);
+  }
   if (count === 0) {
     setCount(6);
   }
 
   return (
     <>
-      <div className="card">
-        <button onClick={shoot}>count is {count}</button>
+      <div>
+        <h1>Russian Roulette</h1>
       </div>
+      <div className="card">
+        <button onClick={shoot}>{btnMsg}</button>
+      </div>
+      <div>{text}</div>
     </>
   );
 }
