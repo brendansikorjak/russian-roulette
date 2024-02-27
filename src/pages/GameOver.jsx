9458c3fd581c66caa9715bb1863ed1bcb3aa20e4
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2024-02-27 17:23:25 -0500

    added a background color animation to Game Over screen

--- src/pages/GameOver.jsx
+++ src/pages/GameOver.jsx
@@ -3,7 +3,11 @@ import '../styles/GameOver.css';
 function GameOver({ reload }) {
   return (
     <div>
-      <style>{'body { background-color: red; }'}</style>
+      <style>
+        {
+          'body { background-color:  #1c0352; animation-name: example; animation-duration: 5s; animation-fill-mode: forwards; }'
+        }
+      </style>
       <div>
         <h1 className="bang">BANG</h1>
       </div>
